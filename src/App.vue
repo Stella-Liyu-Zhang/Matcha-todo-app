851a028b423ba713ca812105578198d62a2d226c
Stella Zhang <l9zhang@ucsd.edu>
2021-07-14 10:44:35 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+     <div class="container">
+            <AddTodo/>
+            <FilterTodos/>
+            <Todos/>
+     </div>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos.vue'
+import AddTodo from './components/AddTodo.vue'
+import FilterTodos from './components/FilterTodos.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Todos,
+    AddTodo,
+    FilterTodos
+  }
+};
+</script>
+
+
+<style>
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #dcedc1;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
